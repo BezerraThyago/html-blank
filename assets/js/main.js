6468f549642e407ec24564b18ddff10b1abcd2ea
BezerraThyago <31627895+BezerraThyago@users.noreply.github.com>
2019-10-17 14:47:46 -0300

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,21 @@
+$(function(){
+
+	// $(window).scroll(function() {    
+	// 	var scroll = $(window).scrollTop();	
+
+	// 	if (scroll >= 400) {
+	// 		$("body").removeClass("scroll");
+	// 		$(".header-menu__content").addClass("mini");
+						
+	// 	} else {
+	// 		$("body").addClass("scroll");	
+	// 		$(".header-menu__content").removeClass("mini");		
+	// 	}
+
+    // });
+    
+    // setTimeout(function(){
+    //     console.log('ok')
+    // }, 1000);
+	 
+});
